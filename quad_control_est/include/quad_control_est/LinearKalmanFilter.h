ac2cb89521350eef840082db02527e9ef5720b07
carp-x <liyuxin_2016@163.com>
2026-03-14 14:54:36 +0800

    quad_control_est: update fun update

--- quad_control_est/include/quad_control_est/LinearKalmanFilter.h
+++ quad_control_est/include/quad_control_est/LinearKalmanFilter.h
@@ -26,16 +26,19 @@ class LinearKalmanFilter : public StateEstimateBase {
   
   const scalar_t foot_radius_ = 0.02;
   vector_t feet_heights_;
+
+  const scalar_t p_scale_init_ = 100.0;
+  const scalar_t q_scale_factor_ = 20.0;
+  const vector3_t g_{0, 0, -9.81};
+  const scalar_t suspect_factor_contact_ = 1.0;
+  const scalar_t suspect_factor_no_contact_ = 100.0;
   
  private:
-  void preparePredict(scalar_t dt, matrix_t& A, matrix_t& B, matrix_t& Q, vector_t& u);
-  // x_new = Ax + Bu, P_new = APA' + Q
-  void predictXP(vector_t& x, matrix_t& P, const matrix_t& A, const matrix_t& B, const matrix_t& Q, const vector_t& u);
-  
-  void prepareUpdate(vector_t& z, matrix_t& H, matrix_t& R);
-  // x += K * (z - H * x_new)
-  void updateXP(vector_t& x, matrix_t& P, const vector_t& z, const matrix_t& H, const matrix_t& R);
-  
+  void dtScaling(const scalar_t dt, matrix_t& A, matrix_t& B, matrix_t& Q);
+  void updateInput(vector_t& u);
+  void updateObserve(vector_t& z);
+  void updateFilter(vector_t& x_hat, matrix_t& P);
+
   size_t num_contacts_, dim_contacts_, num_state_, num_observe_;
   
   matrix_t A_, B_, C_, Q_, P_, R_;
--- quad_control_est/src/LinearKalmanFilter.cpp
+++ quad_control_est/src/LinearKalmanFilter.cpp
@@ -47,19 +47,17 @@ LinearKalmanFilter::LinearKalmanFilter(std::shared_ptr<rclcpp_lifecycle::Lifecyc
   C_.block(0, 6, dim_contacts_, dim_contacts_) = -matrix_t::Identity(dim_contacts_, dim_contacts_);
   
   Q_.setIdentity(num_state_, num_state_);
-  P_ = 100.0 * Q_;
+  P_ = p_scale_init_ * Q_;
   R_.setIdentity(num_observe_, num_observe_);
   feet_heights_.setZero(num_contacts_);
 }
 
 vector_t LinearKalmanFilter::update(const rclcpp::Time& time, const rclcpp::Duration& period) {
-  scalar_t dt = period.seconds();
 
-  preparePredict(dt, A_, B_, Q_, u_);
-  predictXP(x_hat_, P_, A_, B_, Q_, u_);
-
-  prepareUpdate(z_, C_, R_);
-  updateXP(x_hat_, P_, z_, C_, R_);
+  dtScaling(period.toSec(), A_, B_, Q_);
+  updateInput(u_);
+  updateObserve(z_);
+  updateFilter(x_hat_, P_);
 
   updateLinear(x_hat_.head<3>(), x_hat_.segment<3>(3));
   
@@ -68,74 +66,87 @@ vector_t LinearKalmanFilter::update(const rclcpp::Time& time, const rclcpp::Dura
   odom.header.frame_id = "odom";
   odom.child_frame_id = "base";
   publishMsgs(odom);
-  
+
   return rbd_state_;
 }
 
-void LinearKalmanFilter::preparePredict(scalar_t dt, matrix_t& A, matrix_t& B, matrix_t& Q, vector_t& u) {
-
+void LinearKalmanFilter::dtScaling(const scalar_t dt, matrix_t& A, matrix_t& B, matrix_t& Q) {
   A.block<3, 3>(0, 3) = dt * matrix3_t::Identity();
   
   B.block<3, 3>(0, 0) = 0.5 * dt * dt * matrix3_t::Identity();
   B.block<3, 3>(3, 0) = dt * matrix3_t::Identity();
 
-  Q.block<3, 3>(0, 0) = (dt / 20.0) * matrix3_t::Identity() * q_base_pos_;
-  Q.block<3, 3>(3, 3) = (dt * 9.81 / 20.0) * matrix3_t::Identity() * q_base_vel_;
-  Q.block(6, 6, dim_contacts_, dim_contacts_) = dt * matrix_t::Identity(dim_contacts_, dim_contacts_) * q_ee_pos_static_;
-
-  vector3_t g(0, 0, -9.81);
-  u = getRotationMatrixFromZyxEulerAngles(quatToZyx(quat_)) * linear_acc_ + g;
+  Q.block<3, 3>(0, 0) = (dt / q_scale_factor_) * matrix3_t::Identity();
+  Q.block<3, 3>(3, 3) = (dt * std::fabs(g_(2)) / q_scale_factor_) * matrix3_t::Identity();
+  Q.block(6, 6, dim_contacts_, dim_contacts_) = dt * matrix_t::Identity(dim_contacts_, dim_contacts_);
 }
 
-void LinearKalmanFilter::predictXP(vector_t& x, matrix_t& P, const matrix_t& A, const matrix_t& B, const matrix_t& Q, const vector_t& u) {
-  x = A * x + B * u;
-  P = A * P * A.transpose() + Q;
+void LinearKalmanFilter::updateInput(vector_t& u) {
+  u = getRotationMatrixFromZyxEulerAngles(quat_to_zyx(quat_)) * linear_accel_local_ + g_;
 }
 
-void LinearKalmanFilter::prepareUpdate(vector_t& z, matrix_t& /*H*/, matrix_t& R) {
-  const auto& model = pinocchio_interface_.getModel();
+void LinearKalmanFilter::updateObserve(vector_t& z) {
+  auto& model = pinocchio_interface_.getModel();
   auto& data = pinocchio_interface_.getData();
+
   vector_t q_pino = vector_t::Zero(info_.generalizedCoordinatesNum);
+  vector_t v_pino = vector_t::Zero(info_.generalizedCoordinatesNum);
+
   q_pino.segment<3>(3) = rbd_state_.head<3>(); // Only set orientation, let position in origin.
   q_pino.tail(info_.actuatedDofNum) = rbd_state_.segment(6, info_.actuatedDofNum);
-  vector_t v_pino = vector_t::Zero(info_.generalizedCoordinatesNum);
   v_pino.segment<3>(3) = getEulerAnglesZyxDerivativesFromGlobalAngularVelocity<scalar_t>(
-                            q_pino.segment<3>(3),
-                            rbd_state_.segment<3>(info_.generalizedCoordinatesNum)); // Only set angular velocity, let linear velocity be zero
+                          q_pino.segment<3>(3),
+                          rbd_state_.segment<3>(info_.generalizedCoordinatesNum)); // Only set angular velocity, let linear velocity be zero
   v_pino.tail(info_.actuatedDofNum) = rbd_state_.segment(6 + info_.generalizedCoordinatesNum, info_.actuatedDofNum);
-  
+
   pinocchio::forwardKinematics(model, data, q_pino, v_pino);
   pinocchio::updateFramePlacements(model, data);
   const auto ee_pos = ee_kinematics_->getPosition(vector_t());
   const auto ee_vel = ee_kinematics_->getVelocity(vector_t(), vector_t());
-  
-  // 动态构建观测向量与噪声权重
-  for (size_t i = 0; i < num_contacts_; i++) {
-    scalar_t trust = contact_flag_[i] ? 1.0 : 100.0; // 摆动腿不信任度增加
-    
-    // z 观测: [相对位置; 相对速度; 绝对高度]
-    z.segment<3>(3 * i) = -ee_pos[i];
-    z.segment<3>(3 * i)(2) += foot_radius_;
-    z.segment<3>(dim_contacts_ + 3 * i) = -ee_vel[i];
-    z(2 * dim_contacts_ + i) = feet_heights_[i];
 
-    // R 观测噪声 (对角线赋值)
-    R.block<3, 3>(3 * i, 3 * i) = matrix3_t::Identity() * r_ee_pos_ * trust;
-    R.block<3, 3>(dim_contacts_ + 3 * i, dim_contacts_ + 3 * i) = matrix3_t::Identity() * r_ee_vel_ * trust;
-    R(2 * dim_contacts_ + i, 2 * dim_contacts_ + i) = r_ee_height_ * trust;
+  for (size_t i = 0; i < num_contacts_; ++i) {
+    z.segment<3>(3 * i) = -ee_pos[i]; // p - pf
+    z.segment<3>(3 * i)[2] += foot_radius_;
+    z.segment<3>(12 + 3 * i) = -ee_vel[i];
+    z(24 + i) = feet_heights_[i];
   }
 }
 
-void LinearKalmanFilter::updateXP(vector_t& x, matrix_t& P, const vector_t& z, const matrix_t& H, const matrix_t& R) {
-  // S = HPH' + R
-  matrix_t S = H * P * H.transpose() + R;
-  // K = PH'S^-1 (LU分解求解)
-  matrix_t K = P * H.transpose() * S.lu().solve(matrix_t::Identity(num_observe_, num_observe_));
+void LinearKalmanFilter::updateFilter(vector_t& x_hat, matrix_t& P) {
+  auto Q = Q_;
+  auto R = R_;
+
+  Q.block<3, 3>(0, 0) *= q_base_pos_;
+  Q.block<3, 3>(3, 3) *= q_base_vel_;
+  Q.block(6, 6, dim_contacts_, dim_contacts_) *= q_ee_pos_static_;
+
+  R.block(0, 0, dim_contacts_, dim_contacts_) *= r_ee_pos_;
+  R.block(dim_contacts_, dim_contacts_, dim_contacts_, dim_contacts_) *= r_ee_vel_;
+  R.block(2 * dim_contacts_, 2 * dim_contacts_, num_contacts_, num_contacts_) *= r_ee_height_;
+
+  for (size_t i = 0; i < num_contacts_; ++i) {
+    scalar_t suspect_factor = contact_flag_[i] ? suspect_factor_contact_ : suspect_factor_no_contact_;
+
+    size_t start_idx_qe = 6 + 3 * i;
+    size_t start_idx_rp = 3 * i;
+    size_t start_idx_rv = dim_contacts_ + 3 * i;
+    size_t start_idx_rz = 2 * dim_contacts_ + i;
+
+    Q.block<3, 3>(start_idx_qe, start_idx_qe) *= suspect_factor;
+    R.block<3, 3>(start_idx_rp, start_idx_rp) *= suspect_factor;
+    R.block<3, 3>(start_idx_rv, start_idx_rv) *= suspect_factor;
+    R(start_idx_rz, start_idx_rz) *= suspect_factor;
+  }
+
+  x_hat = A_ * x_hat + B_ * u_;
+  P = A_ * P * A_.transpose() + Q;
+
+  auto S = C_ * P * C_.transpose() + R;
+  auto K = P * C_.transpose() * S.lu().solve(matrix_t::Identity(num_observe_, num_observe_));
   
-  // 更新状态与协方差
-  x += K * (z - H * x);
-  P = (matrix_t::Identity(num_state_, num_state_) - K * H) * P;
-  P = (P + P.transpose()) * 0.5; // 对称化
+  x_hat += K * (z_ - C_ * x_hat);
+  P = (matrix_t::Identity(num_state_, num_state_) - K * C_) * P;
+  P = 0.5 * (P + P.transpose()); // ? symmetrization
 }
 
 nav_msgs::msg::Odometry LinearKalmanFilter::getOdomMsg() {
