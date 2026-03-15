501a2bdcbdba05c9d2c84422d803149437a4af9f
carp-x <liyuxin_2016@163.com>
2026-03-15 14:34:24 +0800

    quad_control_mpc: license related

--- quad_control_mpc/src/LeggedRobotPreComputation.cpp
+++ quad_control_mpc/src/LeggedRobotPreComputation.cpp
@@ -1,4 +1,5 @@
 /******************************************************************************
+Copyright (c) 2026, Yuxin Li. All rights reserved.
 Copyright (c) 2020, Farbod Farshidian. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
