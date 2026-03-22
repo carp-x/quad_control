8624b2836ab6655fb0bca6198a504241fdc1b8e9
carp-x <liyuxin_2016@163.com>
2026-03-22 18:13:03 +0800

    quad_control_wbc: license

--- quad_control_wbc/include/quad_control_wbc/Task.h
+++ quad_control_wbc/include/quad_control_wbc/Task.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 Copyright (c) 2026, Yuxin Li. All rights reserved.
-Copyright (c) 2022, QiayuanLiao. All rights reserved.
+Copyright (c) 2022, Qiayuan Liao. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
